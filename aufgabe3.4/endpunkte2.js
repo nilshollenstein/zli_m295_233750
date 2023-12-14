f037ae255e0997ec1cca4322cf5c295a57689c62
Nils Hollenstein <144909884+nilshollenstein@users.noreply.github.com>
2023-12-14 15:36:27 +0000

    chore: curently working on 3.4

---
+++ aufgabe3.4/endpunkte2.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+function getCurrentTimeInTimezone(timezone) {
+	const options = {
+		timeZone: timezone,
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+		hour: '2-digit',
+		minute: '2-digit',
+		second: '2-digit',
+		timeZoneName: 'short',
+	};
+	const formattedTime = new Date().toLocaleString('en-US', options);
+	return formattedTime;
+}
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/now', (req, res) => {
+	const defaultTimezone = 'UTC';
+
+	const timezoneParam = req.query.tz || defaultTimezone;
+
+	const currentTime = getCurrentTimeInTimezone(timezoneParam);
+
+	res.send(`Aktuelle Zeit: ${currentTime}`);
+});
+let namesList = [];
+
+app.post('/names', (req, res) => {
+	namesList.push(req.body.name);
+
+	console.log(namesList);
+	res.send(JSON.stringify(namesList));
+});
+
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
+});
